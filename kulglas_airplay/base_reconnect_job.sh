01b4a75777c7d4de9a2da582c1ae719829387ff1
brarid <47105125+brarid@users.noreply.github.com>
2026-07-11 20:52:15 +0200

    Started on adding logic

--- kulglas_airplay/base_reconnect_job.sh
+++ kulglas_airplay/base_reconnect_job.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 MAC="18:66:96:C5:07:01"
-if ! bluetoothctl info "$MAC" | grep -q "Connected: yes"; then
+iPhone_IP="192.168.1.134"
+iath=`fping $iPhone_IP |grep alive`
+
+
+while read bluetoothctl info "$MAC" | grep -q "Connected: yes"; do
  bluetoothctl connect "$MAC"
-fi
+done
